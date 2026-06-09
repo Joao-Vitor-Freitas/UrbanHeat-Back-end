f50ded437874bc6007d0774a77cafe31495f62d0
João Cortabitart <joaovictorcortabitart@gmail.com>
2026-06-09 17:51:03 -0300

    feat: cors

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import sys
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database.schema import create_tables
 
 from controllers.dashboard_controller import router as dashboard_router
@@ -15,13 +16,23 @@ from controllers.sensor_controller import router as sensor_router
 async def lifespan(app: FastAPI):
     create_tables()
     print("Banco de dados inicializado com sucesso.")
-
     print("Leitura do Arduino desativada para ambiente local.")
-
     yield
 
 
 app = FastAPI(title="UrbanHeat API", lifespan=lifespan)
+
+# --- Configuração do CORS ---
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],  # Permite requisições de qualquer origem (ideal para testar abrindo o index.html direto)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP (GET, POST, etc)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
+# ----------------------------
 
 app.include_router(dashboard_router)
 app.include_router(heatscore_router)
